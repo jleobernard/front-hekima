b0e2628dc10de63916799c86fd05737847776849
junior bernard <junior.bernard-ext@alantaya.com>
2021-11-17 22:56:08 +0100

    - limitation du nombre de cartes - affichage du nombre de cartes total

--- src/pages/quizz/quizz-init.js
+++ src/pages/quizz/quizz-init.js
@@ -2,13 +2,14 @@ import {withRouter} from "react-router-dom";
 import Header from "../../components/header/Header";
 import LoadingMask from "../../components/loading-mask/loading-mask";
 import Toaster from "../../components/Toaster";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {TagsSelector} from "../../components/filter/tags-selector";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import {SourcesSelector} from "../../components/filter/sources-selector";
 import {get} from "../../utils/http";
 import {useHistory} from "react-router-dom/cjs/react-router-dom";
+import {FormControl, TextField} from "@material-ui/core";
 
 
 const QuizzInit = () => {
@@ -16,7 +17,15 @@ const QuizzInit = () => {
   const [error, setError] = useState({msg:"", sev: "info"})
   const [tags, setTags] = useState([])
   const [sources, setSources] = useState([])
+  const [maxCards, setMaxCards] = useState(10)
+  const [count, setCount] = useState(-1)
   const history = useHistory()
+
+  useEffect(() => {
+    get("/api/notes:count", {tags: (tags || []).map(t => t.uri), sources: (sources || []).map(s => s.uri)})
+    .then(count => setCount(count))
+    .catch(count => setCount(-1))
+  }, [tags, sources])
 
   function startQuizz() {
     if(!loading) {
@@ -24,6 +33,9 @@ const QuizzInit = () => {
       setError({msg:"", sev: "info"})
       get("/api/quizz:generate", {tags: (tags || []).map(t => t.uri), sources: (sources || []).map(s => s.uri)})
       .then(notes => {
+        if(maxCards > 0) {
+          notes = notes.slice(0, maxCards)
+        }
         localStorage.setItem("quizz", JSON.stringify(notes))
         history.push("/quizz/run")
       })
@@ -38,8 +50,12 @@ const QuizzInit = () => {
       <form onSubmit={startQuizz} className="form">
         <TagsSelector allowCreation={false} onChange={setTags}/>
         <SourcesSelector allowCreation={false} onChange={setSources} />
+        <FormControl>
+          <TextField id='maxCards' label="Nombre maximums de cartes" value={maxCards} type="number"
+                     min="-1" onChange={e => setMaxCards(e.target.valueAsNumber)} />
+        </FormControl>
         <Button onClick={_ => startQuizz()} color="primary">
-          Démarrer {loading ? <CircularProgress /> : ''}
+          Démarrer ({maxCards} / {count}){loading ? <CircularProgress /> : ''}
         </Button>
       </form>
       <LoadingMask loading={loading}/>
