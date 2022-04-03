00354565716f90ff82b275d6d36472bf9b847632
junior bernard <junior.bernard-ext@alantaya.com>
2022-04-03 17:59:01 +0200

    Passage de la page note d'un composant classe à une fonction pure

--- src/pages/notes/notes.js
+++ src/pages/notes/notes.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {withRouter} from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+import {useHistory, withRouter} from "react-router-dom";
 import {get} from "../../utils/http";
 import "./notes.scss";
 import "../../styles/layout.scss";
@@ -38,100 +38,138 @@ function orDefaultString(str, defaultValue) {
   return defaultValue
 }
 
-class Notes extends React.Component {
+const Notes = () =>  {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      notes: [],
-      filter: {count: 20, offset: 0, initialized: false},
-      creating: false,
-      notification: null,
-      loading: false,
-      hasMoreNotes: true
-    };
-    this.startCreation = this.startCreation.bind(this);
-    this.onDone = this.onDone.bind(this);
-    this.loadMore = this.loadMore.bind(this);
-    this.filterChanged = this.filterChanged.bind(this);
-  }
+  const history = useHistory();
+  const [notes, setNotes] = useState([])
+  const [filter, setFilter] = useState({count: 20, offset: 0})
+  const [needsSearch, setNeedSearch] = useState(false)
+  const [creating, setCreating] = useState(false)
+  const [notification, setNotification] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [hasMoreNotes, setHasMoreNotes] = useState(true)
+  const [error, setError] = useState("")
+  const [raz, setRaz] = useState(true)
 
-  componentDidMount() {
-    this.setState({loading: true})
-    //this.refreshNotes(true)
-  }
+  useEffect(() => {
+    loadFilterFromURL().then(_filter => {
+      setFilter(_filter)
+      setNeedSearch(true)
+    })
+  }, [])
 
-  filterChanged(newFilter) {
+  useEffect(() => {
+    if(needsSearch) {
+      const params = new URLSearchParams(history.location.search)
+      const _filterForSearch = getFilter(filter)
+      get("/api/notes", _filterForSearch).then(_notes => {
+        setNotes(raz ? _notes : [...notes, ..._notes])
+        setHasMoreNotes(_notes && _notes.length > 0)
+        setFilter(filter)
+        setLoading(false)
+        setNeedSearch(false)
+        seekNote(params.get('note'))
+        setRaz(false)
+      }).catch(err => {
+        console.error(err)
+        setError("Erreur lors de la recherche de notes")
+        setNeedSearch(false)
+      })
+    }
+  }, [needsSearch]);
+
+  function filterChanged(newFilter) {
     const updated = {
       count: 20,
       offset: 0,
-      initialized: true,
       ...newFilter
     }
-    this.setState({filter: updated});
-    //this.refreshNotes(true, updated)
-    this.updateRouteParams(updated)
+    updateRouteParams(updated)
+    setRaz(true)
+    setFilter(updated);
+    setNeedSearch(true)
   }
 
-  updateRouteParams(newFilter) {
-    const _filter = newFilter || this.state.filter
+  function loadFilterFromURL() {
+    const promises = []
+    const params = new URLSearchParams(history.location.search)
+    const src = params.get('src')
+    if(src) {
+      promises.push(get(`/api/sources/${src}`))
+    }
+    const withTagsUri = (params.get('tags') || '').split(',');
+    const withoutTagsUri = (params.get('notTags') || '').split(',');
+    [...withTagsUri, ...withoutTagsUri]
+    .filter(uri => !!uri)
+    .forEach(uri => promises.push(get(`/api/tags/${uri}`)))
+    const _filter = {
+      ...filter,
+      q: orDefaultString(params.get('q'), ''),
+      count: orDefault(params.get('count'), 20),
+      offset: orDefault(params.get('offset'), 0)
+    }
+    let promiseLoadAll;
+    if(promises.length > 0) {
+      promiseLoadAll = new Promise((resolve) => {
+        Promise.all(promises).then(responses => {
+          let beginTags;
+          if(src) {
+            _filter.source = responses[0]
+            beginTags = 1
+          } else {
+            beginTags = 0
+          }
+          const fetchedTags = responses.slice(beginTags)
+          _filter.tags = withTagsUri.map(wt => lodash.find(fetchedTags, t => t.uri === wt)).filter(u => !!u)
+          _filter.notTags = withoutTagsUri.map(wt => lodash.find(fetchedTags, t => t.uri === wt)).filter(u => !!u)
+          resolve(_filter)
+        }).catch(() => resolve(_filter))
+      })
+    } else {
+      promiseLoadAll = new Promise((resolve) => resolve(_filter))
+    }
+    return promiseLoadAll
+  }
+
+  function updateRouteParams(__filter) {
+    const _filter = __filter || filter
     const src = _filter.source ? _filter.source.uri : ''
     const tags = (_filter.tags || []).map(t => t.uri).join(',')
+    const notTags = (_filter.notTags || []).map(t => t.uri).join(',')
     const q = (_filter.q || '').trim()
-    this.props.history.push(`/notes?count=${_filter.count}&offset=${_filter.offset}&src=${src}&tags=${tags}&q=${encodeURIComponent(q)}`)
+    history.push(`/notes?count=${_filter.count}&offset=${_filter.offset}&src=${src}&tags=${tags}&notTags=${notTags}&q=${encodeURIComponent(q)}`)
   }
 
-  refreshNotes(override = false, filter = null) {
-    const _filter = this.getFilter(filter || this.state.filter);
-    this.setState({loading: true});
-    get('/api/notes', _filter)
-    .then(notes => {
-      let newNotes;
-      if(override) {
-        newNotes = notes;
-      } else {
-        newNotes = this.state.notes.concat(notes);
-      }
-      this.setState({notes: newNotes, hasMoreNotes: notes && notes.length > 0})
-    })
-    .catch(err => this.setState({error : "Erreur à la récupération des notes : " + err}))
-    .finally(() => {
-      this.setState({loading: false});
-    })
-  }
-  getFilter(filter) {
+  function getFilter(__filter) {
     const _filter = {
-      offset: filter.offset,
-      count: filter.count
+      offset: raz ? 0 : __filter.offset,
+      count: raz ? __filter.count + __filter.offset : __filter.count
     };
-    if(filter.source) {
-      _filter.source = filter.source.uri;
+    if(__filter.source) {
+      _filter.source = __filter.source.uri;
     }
-    if(filter.tags) {
-      _filter.tags = lodash.map(filter.tags, t => t.uri);
+    if(__filter.tags) {
+      _filter.tags = lodash.map(__filter.tags, t => t.uri);
     }
-    if(filter.q) {
-      _filter.q = filter.q
+    if(__filter.notTags) {
+      _filter.notTags = lodash.map(__filter.notTags, t => t.uri);
+    }
+    if(__filter.q) {
+      _filter.q = __filter.q
     }
     return _filter;
   }
 
-  startCreation() {
-    this.setState({creating: true});
+  function startCreation() {
+    setCreating(true)
   }
 
-
-  showNotification(notification) {
-    this.setState({notification});
-    setTimeout(() => this.setState({notification: null}), 3000);
-  }
-
-  getListItem(note) {
+  function getListItem(note) {
     return <li>
       <Card className={"note-card"}>
         <NoteFilesDisplay note={note} />
         {note.subs  && note.subs.length > 0 ? <VideoThumbnailList title="" videos={note.subs} />: <></>}
-        <CardContent onClick={() => this.navigateToNote(note)}>
+        <CardContent onClick={() => navigateToNote(note)}>
           <Typography component="p" className={"note-text"} gutterBottom={true}>
             <ReactMarkdown remarkPlugins={[gfm]} rehypePlugins={[rehypeRaw]} children={note.valeur}/>
           </Typography>
@@ -150,157 +188,76 @@ class Notes extends React.Component {
     </li>
   }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    const prevFilter = prevState.filter;
-    const filter = this.state.filter;
-    if(!filter.initialized) {
-      const promises = []
-      const params = new URLSearchParams(this.props.history.location.search)
-      const src = params.get('src')
-      if(src) {
-        promises.push(get('/api/sources/'+src))
-      }
-      (params.get('tags') || '').split(',').filter(uri => uri).forEach(uri => promises.push(get('/api/tags/'+uri)))
-      const _filter = {
-        ...filter,
-        initialized: true,
-        q: orDefaultString(params.get('q'), ''),
-        count: orDefault(params.get('count'), 20),
-        offset: orDefault(params.get('offset'), 0)
-      }
-      let promiseLoadAll;
-      if(promises.length > 0) {
-        promiseLoadAll = new Promise((resolve) => {
-          Promise.all(promises).then(responses => {
-            let beginTags;
-            if(src) {
-              _filter.source = responses[0]
-              beginTags = 1
-            } else {
-              beginTags = 0
-            }
-            if(beginTags < promises.length - 1) {
-              _filter.tags = responses.subarray(beginTags)
-            }
-            resolve()
-          }).catch(() => resolve())
-        })
-      } else {
-        promiseLoadAll = new Promise((resolve) => resolve())
-      }
-      promiseLoadAll.then(() => {
-        const nbToLoad = _filter.offset + _filter.count
-        if(nbToLoad > 0) {
-          const _filterForSearch = this.getFilter(_filter)
-          _filterForSearch.count = nbToLoad
-          _filterForSearch.offset = 0
-          get("/api/notes", _filterForSearch).then(notes => {
-            this.setState({
-              notes,
-              hasMoreNotes: notes && notes.length > 0,
-              filter: _filter,
-              loading: false
-            })
-            this.seekNote(params.get('note'))
-          })
-        } else {
-          this.setState({
-            notes: [],
-            hasMoreNotes: true,
-            filter: _filter,
-            loading: false
-          })
-        }
-      }).catch(() => this.setState({loading: false}))
-    } else if(prevFilter && filter && prevFilter.initialized && this.hasChanged(filter, prevFilter)) {
-      if(filter.offset > prevFilter.offset) {
-        this.refreshNotes(false);
-      } else {
-        this.refreshNotes(true);
-      }
-      this.updateRouteParams()
-    }
-  }
-  hasChanged(filter, prevFilter) {
-    return filter.offset !== prevFilter.offset ||
-        filter.count !== prevFilter.count ||
-        filter.source  !== prevFilter.source ||
-        filter.tags  !== prevFilter.tags ||
-        filter.q !== prevFilter.q
-  }
-
-  seekNote(noteUri) {
+  function seekNote(noteUri) {
     if(noteUri) {
       setTimeout(() => {
         const elt = document.getElementById(noteUri);
         if(elt) {
           elt.scrollIntoView()
         } else {
-          this.seekNote(noteUri)
+          seekNote(noteUri)
         }
       }, 100)
     }
   }
 
-  onDone(note) {
+  function onDone(note) {
     if(note) {
-      const newNotes = [...this.state.notes];
+      const newNotes = [...notes];
       const index = lodash.findIndex(newNotes, n => n.uri === note.uri);
       if(index >=0) {
         newNotes[index] = note;
       } else {
         newNotes.unshift(note);
       }
-      this.setState({
-        notification: 'Note sauvegardée',
-        notes: newNotes
-      });
+      setNotification('Note sauvegardée')
+      setNotes(newNotes)
     }
-    this.setState({creating: false});
+    setCreating(false)
   }
 
-  loadMore() {
-    this.setState({filter: {
-        ...this.state.filter,
-        offset: this.state.filter.offset+20
-      }})
+  function loadMore() {
+    const newFilter = {
+      ...filter,
+      offset: filter.offset+20
+    }
+    setFilter(newFilter)
+    updateRouteParams(newFilter)
+    setNeedSearch(true)
   }
 
-  render() {
-    const notes = this.state.notes;
-    return (
-      <div className="app">
-        <AddToHomeScreen appId="com.leo.notes" />
-        <Header title="Notes" goBack={false} withSearch={true} filterChanged={this.filterChanged}/>
-        <List className="notes-list">
-          <ListItem key="spinner-loading-first" className="centered-item">
-            {this.state.loading? <CircularProgress /> : ''}
-          </ListItem>
-          {notes.map(elt => <ListItem key={elt.uri} id={elt.uri}>{this.getListItem(elt)}</ListItem>)}
-          {this.state.hasMoreNotes && !this.state.loading ? <ListItem className="centered-item" key="load-more">
-            <Button size="small" color="primary" onClick={() => this.loadMore()}>Voir plus</Button>
-          </ListItem> : <></>}
-          <ListItem key="spinner-loading" className="centered-item">
-            {this.state.loading? <CircularProgress /> : ''}
-          </ListItem>
-        </List>
-        {this.state.creating ? <NoteCreation creating={this.state.creating} onDone={this.onDone}/> : <></>}
-        <Toaster error={this.state.error}/>
-        <Toaster error={this.state.notification} severity="info"/>
-        <Fab color="primary" aria-label="add" className="fab" onClick={() => this.startCreation()}>
-          <AddIcon />
-        </Fab>
-      </div>
-    );
-  }
-
-  navigateToNote(note) {
-    const _filter = this.state.filter || {}
+  function navigateToNote(note) {
+    const _filter = filter || {}
     const src = _filter.source ? _filter.source.uri : ''
     const tags = (_filter.tags || []).map(t => t.uri).join(',')
-    this.props.history.push(`/notes/${note.uri}?count=${_filter.count}&offset=${_filter.offset}&src=${src}&tags=${tags}`)
+    const notTags = (_filter.notTags || []).map(t => t.uri).join(',')
+    history.push(`/notes/${note.uri}?count=${_filter.count}&offset=${_filter.offset}&src=${src}&tags=${tags}&notTags=${notTags}`)
     return undefined;
   }
+  return (
+    <div className="app">
+      <AddToHomeScreen appId="com.leo.notes" />
+      <Header title="Notes" goBack={false} withSearch={true} filterChanged={filterChanged}/>
+      <List className="notes-list">
+        <ListItem key="spinner-loading-first" className="centered-item">
+          {loading? <CircularProgress /> : ''}
+        </ListItem>
+        {notes.map(elt => <ListItem key={elt.uri} id={elt.uri}>{getListItem(elt)}</ListItem>)}
+        {hasMoreNotes && !loading ? <ListItem className="centered-item" key="load-more">
+          <Button size="small" color="primary" onClick={() => loadMore()}>Voir plus</Button>
+        </ListItem> : <></>}
+        <ListItem key="spinner-loading" className="centered-item">
+          {loading? <CircularProgress /> : ''}
+        </ListItem>
+      </List>
+      {creating ? <NoteCreation creating={creating} onDone={onDone}/> : <></>}
+      <Toaster error={error}/>
+      <Toaster error={notification} severity="info"/>
+      <Fab color="primary" aria-label="add" className="fab" onClick={() => startCreation()}>
+        <AddIcon />
+      </Fab>
+    </div>
+  );
 }
 
 export default withRouter(Notes);
