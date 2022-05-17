9ee1bb9a77e20c7286b6b5bda63f1948f3acef91
Junior Léo Bernard <jleobernard@gmail.com>
2022-05-17 12:35:00 +0200

    Conf pour déployer dans un container

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+NOTES_FRONT_VERSION=1.0.0
 cd $DIR/../
+rm -Rf build
 set PUBLIC_URL=https://notes.jleo.tech && set REACT_APP_API_ROOT_URL=https://notes.jleo.tech && npm run build
 find build -name '.DS_Store' -exec rm -f {} \;
-echo "Cleaning previous version"
-ssh mandela "rm -Rf /opt/containers/nginx/data/nginx/html/notes/*"
-echo "Copying new version"
-scp -r build/* mandela:/opt/containers/nginx/data/nginx/html/notes/
-echo "Reloading nginx"
-ssh mandela "cd /opt/containers/nginx && ./reload.sh"
+docker build -t jleobernard/notes-front:$NOTES_FRONT_VERSION --no-cache -f ./.docker/Dockerfile .
+docker push jleobernard/notes-front:$NOTES_FRONT_VERSION
