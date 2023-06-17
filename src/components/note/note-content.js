5967b91da494d25d503c0ef5bb124196e2e68f59
Junior Léo Bernard <jleobernard@gmail.com>
2023-06-18 00:12:02 +0200

    feat: style editor menu bar and add options

--- src/components/note/note-content.js
+++ src/components/note/note-content.js
@@ -3,8 +3,17 @@ import StarterKit from '@tiptap/starter-kit'
 import { Color } from '@tiptap/extension-color'
 import ListItem from '@tiptap/extension-list-item'
 import TextStyle from '@tiptap/extension-text-style'
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+
 import React from 'react';
 import EditorMenuBar from 'components/rte/editor-menu-bar';
+import './note-content.scss';
 
 const NoteContent = ({note, readOnly, onBlur}) => {
   function handleOnBlur(event) {
@@ -23,8 +32,9 @@ const NoteContent = ({note, readOnly, onBlur}) => {
   }
   const editor = useEditor({
     extensions: [
+      Document, Paragraph, Text, TextStyle,// Color,
       Color.configure({ types: [TextStyle.name, ListItem.name] }),
-      TextStyle.configure({ types: [ListItem.name] }),
+      //TextStyle.configure({ types: [ListItem.name] }),
       StarterKit.configure({
         bulletList: {
           keepMarks: true,
@@ -35,6 +45,12 @@ const NoteContent = ({note, readOnly, onBlur}) => {
           keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
         },
       }),
+      Table.configure({
+        resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
     ],
     content: getContent(note),
     editable: !readOnly
--- src/components/rte/editor-menu-bar.js
+++ src/components/rte/editor-menu-bar.js
@@ -1,14 +1,38 @@
 import React from 'react'
+import './editor-menu-bar.scss'
+import FormatBoldIcon from '@mui/icons-material/FormatBold';
+import FormatItalicIcon from '@mui/icons-material/FormatItalic';
+import StrikethroughSIcon from '@mui/icons-material/StrikethroughS';
+import CodeIcon from '@mui/icons-material/Code';
+import TitleIcon from '@mui/icons-material/Title';
+import TableChartIcon from '@mui/icons-material/TableChart';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
+import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
+import UndoIcon from '@mui/icons-material/Undo';
+import RedoIcon from '@mui/icons-material/Redo';
 
 const EditorMenuBar = ({ editor }) => {
+  const [secondaryBar, setSecondaryBar] = React.useState('')
+  
+  function toggleSecondaryBar(mode) {
+    if(secondaryBar === mode) {
+      setSecondaryBar('')
+    } else {
+      setSecondaryBar(mode)
+    }
+  }
+
   if (!editor) {
     return null
   }
 
-  return (
-    <>
-      <button
-        onClick={() => editor.chain().focus().toggleBold().run()}
+  function renderMainBar() {
+    return (<>
+    <button type="button"
+        onClick={() => {editor.chain().focus().toggleBold().run(); setSecondaryBar('') }}
         disabled={
           !editor.can()
             .chain()
@@ -18,9 +42,9 @@ const EditorMenuBar = ({ editor }) => {
         }
         className={editor.isActive('bold') ? 'is-active' : ''}
       >
-        bold
+        <FormatBoldIcon fontSize='small'/>
       </button>
-      <button
+      <button type="button"
         onClick={() => editor.chain().focus().toggleItalic().run()}
         disabled={
           !editor.can()
@@ -31,9 +55,9 @@ const EditorMenuBar = ({ editor }) => {
         }
         className={editor.isActive('italic') ? 'is-active' : ''}
       >
-        italic
+        <FormatItalicIcon fontSize='small' />
       </button>
-      <button
+      <button type="button"
         onClick={() => editor.chain().focus().toggleStrike().run()}
         disabled={
           !editor.can()
@@ -44,9 +68,9 @@ const EditorMenuBar = ({ editor }) => {
         }
         className={editor.isActive('strike') ? 'is-active' : ''}
       >
-        strike
+        <StrikethroughSIcon fontSize='small' />
       </button>
-      <button
+      <button type="button"
         onClick={() => editor.chain().focus().toggleCode().run()}
         disabled={
           !editor.can()
@@ -57,87 +81,38 @@ const EditorMenuBar = ({ editor }) => {
         }
         className={editor.isActive('code') ? 'is-active' : ''}
       >
-        code
+        <CodeIcon fontSize='small' />
       </button>
-      <button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
-        clear marks
-      </button>
-      <button onClick={() => editor.chain().focus().clearNodes().run()}>
-        clear nodes
-      </button>
-      <button
-        onClick={() => editor.chain().focus().setParagraph().run()}
-        className={editor.isActive('paragraph') ? 'is-active' : ''}
-      >
-        paragraph
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-        className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
-      >
-        h1
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-        className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
-      >
-        h2
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-        className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
-      >
-        h3
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
-        className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}
-      >
-        h4
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
-        className={editor.isActive('heading', { level: 5 }) ? 'is-active' : ''}
-      >
-        h5
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
-        className={editor.isActive('heading', { level: 6 }) ? 'is-active' : ''}
-      >
-        h6
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleBulletList().run()}
-        className={editor.isActive('bulletList') ? 'is-active' : ''}
-      >
-        bullet list
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleOrderedList().run()}
-        className={editor.isActive('orderedList') ? 'is-active' : ''}
-      >
-        ordered list
-      </button>
-      <button
+      <button type="button"
         onClick={() => editor.chain().focus().toggleCodeBlock().run()}
         className={editor.isActive('codeBlock') ? 'is-active' : ''}
       >
-        code block
+        <DeveloperModeIcon fontSize='small' />
       </button>
-      <button
+      <button type="button" onClick={() => toggleSecondaryBar('h')}><TitleIcon fontSize='small' /></button>
+      <button type="button" onClick={() => toggleSecondaryBar('table')}><TableChartIcon fontSize='small' /></button>
+      <button type="button"
+        onClick={() => editor.chain().focus().toggleBulletList().run()}
+        className={editor.isActive('bulletList') ? 'is-active' : ''}
+      >
+        <FormatListBulletedIcon fontSize='small' />
+      </button>
+      <button type="button"
+        onClick={() => editor.chain().focus().toggleOrderedList().run()}
+        className={editor.isActive('orderedList') ? 'is-active' : ''}
+      >
+        <FormatListNumberedIcon fontSize='small' />
+      </button>
+      <button type="button"
         onClick={() => editor.chain().focus().toggleBlockquote().run()}
         className={editor.isActive('blockquote') ? 'is-active' : ''}
       >
-        blockquote
+        <FormatQuoteIcon fontSize='small' />
       </button>
-      <button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
-        horizontal rule
+      <button type="button" onClick={() => editor.chain().focus().setHorizontalRule().run()}>
+        <HorizontalRuleIcon  fontSize='small' />
       </button>
-      <button onClick={() => editor.chain().focus().setHardBreak().run()}>
-        hard break
-      </button>
-      <button
+      <button type="button"
         onClick={() => editor.chain().focus().undo().run()}
         disabled={
           !editor.can()
@@ -147,9 +122,9 @@ const EditorMenuBar = ({ editor }) => {
             .run()
         }
       >
-        undo
+        <UndoIcon  fontSize='small' />
       </button>
-      <button
+      <button type="button"
         onClick={() => editor.chain().focus().redo().run()}
         disabled={
           !editor.can()
@@ -159,15 +134,123 @@ const EditorMenuBar = ({ editor }) => {
             .run()
         }
       >
-        redo
+      <RedoIcon  fontSize='small' />
       </button>
-      <button
-        onClick={() => editor.chain().focus().setColor('#958DF1').run()}
-        className={editor.isActive('textStyle', { color: '#958DF1' }) ? 'is-active' : ''}
+      <input className='color-button'
+        type="color"
+        onInput={event => editor.chain().focus().setColor(event.target.value).run()}
+        value={editor.getAttributes('textStyle').color}
+      />
+    </>)
+  }
+
+  function renderHs() {
+    return (
+      <>
+      {secondaryBar === 'h' ? <> 
+        <button type="button"
+          onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+          className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
+        >
+          H1
+        </button>
+        <button type="button"
+          onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+          className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
+        >
+          H2
+        </button>
+        <button type="button"
+          onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+          className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
+        >
+          H3
+        </button>
+        <button type="button"
+          onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+          className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}
+        >
+          H4
+        </button>
+        <button type="button"
+          onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
+          className={editor.isActive('heading', { level: 5 }) ? 'is-active' : ''}
+        >
+          H5
+        </button>
+        <button type="button"
+          onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
+          className={editor.isActive('heading', { level: 6 }) ? 'is-active' : ''}
+        >
+          H6
+        </button>
+      </> : <></>}
+      </>
+    )
+  }
+
+  function renderTable() {
+    return <>
+    {secondaryBar === 'table' ? 
+    <>
+      <button type='button'
+        onClick={() => editor.chain().focus().insertTable({ rows: 3, cols: 3, withHeaderRow: true }).run()
+        }
       >
-        purple
+        Nouveau tableau
+      </button>
+      <button type="button" onClick={() => editor.chain().focus().addColumnBefore().run()}>
+        + colonne avant
+      </button>
+      <button type="button" onClick={() => editor.chain().focus().addColumnAfter().run()}>+ colonne après</button>
+      <button type="button" onClick={() => editor.chain().focus().deleteColumn().run()}>- colonne</button>
+      <button type="button" onClick={() => editor.chain().focus().addRowBefore().run()}>+ ligne avant</button>
+      <button type="button" onClick={() => editor.chain().focus().addRowAfter().run()}>+ ligne après</button>
+      <button type="button" onClick={() => editor.chain().focus().deleteRow().run()}>- ligne</button>
+      <button type="button" onClick={() => editor.chain().focus().deleteTable().run()}>- tableau</button>
+      <button type="button" onClick={() => editor.chain().focus().mergeCells().run()}>Fusionner cellules</button>
+      <button type="button" onClick={() => editor.chain().focus().splitCell().run()}>Séparer celulles</button>
+      <button type="button" onClick={() => editor.chain().focus().toggleHeaderColumn().run()}>
+        En-tête colonne
+      </button>
+      <button type="button" onClick={() => editor.chain().focus().toggleHeaderRow().run()}>
+      En-tête ligne
+      </button>
+      <button type="button" onClick={() => editor.chain().focus().toggleHeaderCell().run()}>
+      En-tête cellule
+      </button>
+      <button type="button" onClick={() => editor.chain().focus().mergeOrSplit().run()}>Fusionner/séparer</button>
+      <button type="button" onClick={() => editor.chain().focus().fixTables().run()}>Réparer</button>
+      <button type="button" onClick={() => editor.chain().focus().goToNextCell().run()}>Cellule suivante</button>
+      <button type="button" onClick={() => editor.chain().focus().goToPreviousCell().run()}>
+        Cellule précédente
       </button>
     </>
+    : <></>}
+    </>
+  }
+
+  function renderSecondaryBar() {
+
+    return (
+      <>
+      {renderHs()}
+      {renderTable()}
+      </>
+    )
+  }
+
+  return (
+    <div>
+      <div className="editor-button-bar main-editor-button-bar">
+        {renderMainBar()}
+      </div>
+      {secondaryBar ? 
+      <div className="editor-button-bar secondary-editor-button-bar">
+        {renderSecondaryBar()}
+      </div> : <></>}
+      
+    </div>
   )
 }
 
